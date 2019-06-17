b59aa5d414294904d87812ae2c23f26e6da6fddc
Siddharth Verma <siddharthverma_911@yahoo.co.in>
2019-06-17 12:52:41 +0530

    Detect bluetooth for API's above 26
    Added an extra validation for bluetooth headsets for API's above 26.

--- Android/src/com/davikingcode/DetectHeadset/DetectHeadset.java
+++ Android/src/com/davikingcode/DetectHeadset/DetectHeadset.java
@@ -8,13 +8,23 @@ public class DetectHeadset {
 	static AudioManager myAudioManager;
 	
 	public DetectHeadset(Context context) {
-		
 		myAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 	}
 	
 	public boolean _Detect() {
-		if(myAudioManager.isWiredHeadsetOn() || myAudioManager.isBluetoothA2dpOn()){
+		//Added validation for newer api's above 26.
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+		    AudioDeviceInfo[] audioDeviceInfos = myAudioManager.getDevices(AudioManager.GET_DEVICES_INPUTS);
+		    for (int i=0;i<audioDeviceInfos.length; i++){
+			if(audioDeviceInfos[i].getType() == AudioDeviceInfo.TYPE_BLUETOOTH_SCO ||
+				audioDeviceInfos[i].getType() == AudioDeviceInfo.TYPE_WIRED_HEADSET)
+			    return true;
+		    }
+		}else {
+		    //This should work as expected for the older api's
+		    if(myAudioManager.isWiredHeadsetOn() || myAudioManager.isBluetoothA2dpOn())
 			return true;
-		}else return false;
-	}
+		}
+		return false;
+    	}
 }
