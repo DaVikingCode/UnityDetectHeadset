54fef465dd1cd4025943865eb4ccf2a6969977d5
Aymeric Lamboley <aymeric.lamboley@gmail.com>
2020-04-30 17:41:43 +0200

    close #4 DllImport only for on iOS

--- Assets/Plugins/DetectHeadset.cs
+++ Assets/Plugins/DetectHeadset.cs
@@ -2,24 +2,26 @@
 using System.Collections;
 using System.Runtime.InteropServices;
 
-public class DetectHeadset {
+public class DetectHeadset
+{
+	#if UNITY_IOS && !UNITY_EDITOR
+		[DllImport ("__Internal")]
+		static private extern bool _Detect();
+	#endif
 
-	[DllImport ("__Internal")]
-	static private extern bool _Detect();
-
-	static public bool Detect() {
-
+	static public bool Detect()
+	{
 		#if UNITY_IOS  && !UNITY_EDITOR
 			return _Detect();
 
 		#elif UNITY_ANDROID && !UNITY_EDITOR
 
-			using (var javaUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
-
-				using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity")) {
-
-					using (var androidPlugin = new AndroidJavaObject("com.davikingcode.DetectHeadset.DetectHeadset", currentActivity)) {
-
+			using (var javaUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+			{
+				using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+				{
+					using (var androidPlugin = new AndroidJavaObject("com.davikingcode.DetectHeadset.DetectHeadset", currentActivity))
+					{
 						return androidPlugin.Call<bool>("_Detect");
 					}
 				}
